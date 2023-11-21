5a55c6ac9be31c29ec8c8904afde552b89dfcbdc
lokesh shriwas <lokeshshriwas2802@gmail.com>
2023-11-21 14:37:25 +0530

    about page uploaded

--- script.js
+++ script.js
@@ -9,3 +9,30 @@ gsap.to(".headpara h1",{
         scrub: 2,
     }
 })
+
+
+gsap.from("#storycontainer #storyinfo p",{
+    transform: "translateX(-1000%)",
+    stagger: 3,
+    opacity: 0,
+    scrollTrigger:{
+        trigger: ".page2",
+        scoller: "body",
+        start: "top 0",
+        duration: 3,
+        scrub: 1,
+        pin: true
+    }
+})
+
+gsap.from("#storycontainer img",{
+    opacity: 0,
+    scale: 0,
+    scrollTrigger:{
+        trigger: ".page2",
+        scoller: "body",
+        start: "top 1",
+        scrub: 1,
+        pin: true
+    }
+})
