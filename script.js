4c174a33e77222d2814eae080dc6ba7b525c3caf
lokesh shriwas <lokeshshriwas2802@gmail.com>
2023-11-21 18:42:53 +0530

    added animations and mouse follower

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+Shery.mouseFollower();
+Shery.makeMagnet("a, img")
+
 gsap.to(".headpara p",{
     transform: "translateX(-80%)",
     color: "black",
@@ -35,3 +38,27 @@ gsap.from("#storycontainer img",{
         scrub: 1,
     }
 })
+
+gsap.from(".pg3part1 h1",{
+    transform: "translateX(-100%)",
+    color: "black",
+    scrollTrigger:{
+        trigger: ".page3",
+        scoller: "body",
+        start: "top 1",
+        scrub: 1,
+        pin: true,
+    }
+})
+
+gsap.from(".pg3part2 h1",{
+    transform: "translateX(100%)",
+    color: "black",
+    scrollTrigger:{
+        trigger: ".page3",
+        scoller: "body",
+        start: "top 1",
+        scrub: 1,
+    }
+})
+
